8a181e64ff2b7e6c3447704419eee0b819dddd6b
Larissa Voigt <hi@larissavoigt.com>
2016-04-07 16:49:38 -0700

    Role Claim ViewModel

---
+++ Assigment8/Controllers/RoleClaim_vm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assigment8.Controllers
+{
+    public class RoleClaimBase
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, StringLength(150)]
+        public string Name { get; set; }
+    }
+}
