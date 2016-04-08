45c15526fbcf599b550a987393b24e054587953f
Larissa Voigt <hi@larissavoigt.com>
2016-04-07 17:36:53 -0700

    Seed users

--- Assigment8/Migrations/Configuration.cs
+++ Assigment8/Migrations/Configuration.cs
@@ -1,6 +1,10 @@
 namespace Assigment8.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -14,18 +18,6 @@ namespace Assigment8.Migrations
 
         protected override void Seed(Assigment8.Models.ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
             context.RoleClaims.AddOrUpdate(
                 r => r.Name,
                 new Models.RoleClaim { Name = "Executive" },
@@ -35,6 +27,39 @@ namespace Assigment8.Migrations
                 new Models.RoleClaim { Name = "Intern" },
                 new Models.RoleClaim { Name = "Viewer" }
             );
+
+            AddOrUpdateUser(context, "executive@senecajukebox.com", new List<string> { "Executive", "Coordinator", "Clerk", "Staff" });
+            AddOrUpdateUser(context, "coordinator@senecajukebox.com", new List<string> { "Coordinator", "Clerk", "Staff" });
+            AddOrUpdateUser(context, "clerk@senecajukebox.com", new List<string> { "Clerk", "Staff" });
+            AddOrUpdateUser(context, "staff@senecajukebox.com", new List<string> { "Staff" });
+        }
+
+        void AddOrUpdateUser(Assigment8.Models.ApplicationDbContext context, string email, List<string> roles)
+        {
+            foreach (var role in roles)
+            {
+                if (!context.Roles.Any(r => r.Name == role))
+                {
+                    var store = new RoleStore<IdentityRole>(context);
+                    var manager = new RoleManager<IdentityRole>(store);
+                    manager.Create(new IdentityRole { Name = role });
+                }
+            }
+
+            if (!context.Users.Any(u => u.UserName == email))
+            {
+                var store = new UserStore<ApplicationUser>(context);
+                var manager = new UserManager<ApplicationUser>(store);
+                var user = new ApplicationUser { UserName = email };
+
+                manager.Create(user, "Passw0rd!");
+
+                foreach (var role in roles)
+                {
+                    manager.AddToRole(user.Id, role);
+                }
+
+            }
         }
     }
 }
